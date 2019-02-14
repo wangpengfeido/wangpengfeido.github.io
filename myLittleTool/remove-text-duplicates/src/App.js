25fcdc9aa462601bd6c72f6b8427ac86f1cb2ecb
Wangpengfei <1036793130@qq.com>
2019-02-14 18:21:41 +0800

    ...

--- myLittleTool/remove-text-duplicates/src/App.js
+++ myLittleTool/remove-text-duplicates/src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import {MainPage} from "./components/main-page/main-page";
+
 class App extends Component {
   constructor() {
     super();
@@ -11,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        asdf
+        <MainPage/>
       </div>
     );
   }
--- myLittleTool/remove-text-duplicates/src/components/main-page/main-page.js
+++ myLittleTool/remove-text-duplicates/src/components/main-page/main-page.js
@@ -1,7 +1,54 @@
 import React, {Component} from 'react';
-import './main-page.css';
+import './main-page.scss';
 
+export class MainPage extends Component {
+  constructor() {
+    super();
 
+    this.state = {};
+
+    this.ref_textarea_source = React.createRef();
+    this.ref_textarea_result = React.createRef();
+  }
+
+  transformTextToArray(text) {
+    let result = this.ref_textarea_source.current.value.split('\n');
+    result.forEach(item => {
+      item.trim();
+    });
+    result = result.filter(item => {
+      return !!item;
+    });
+    return result
+  }
+
+  handleDuplicateClick = () => {
+    const array_fragment = this.transformTextToArray(this.ref_textarea_source.current.value.split('\n'));
+    const array_duplicatedFragment = Array.from(new Set(array_fragment));
+    this.ref_textarea_result.current.value = array_duplicatedFragment.join('\n');
+  };
+
+  render() {
+    return (<div className='main-page'>
+      <div>
+        <div>
+          <button onClick={() => {this.handleDuplicateClick()}}>去重</button>
+        </div>
+        <div>
+          <label><input type='checkbox'/>高级（本功能尚未开发）</label>
+        </div>
+      </div>
+      <div className='content'>
+        <div className='content__source'>
+          <textarea ref={this.ref_textarea_source}></textarea>
+        </div>
+        <div className='content__result'>
+          <textarea ref={this.ref_textarea_result}></textarea>
+        </div>
+      </div>
+    </div>);
+  }
+}
 
 
 
